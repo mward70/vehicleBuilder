e19e9457ae422f882c69b3d31282ed0061e5ae96
Megan Ward <wardmeg1998@gmail.com>
2025-01-14 21:41:35 -0500

    completed toDo items in motorbike.ts

--- Develop/src/classes/Motorbike.ts
+++ Develop/src/classes/Motorbike.ts
@@ -43,6 +43,9 @@ class Motorbike extends Vehicle {
       }
     }
   // TODO: Implement the wheelie method
+  wheelie(): void{
+    console.log(`Motorbike ${this.make} ${this.model} is doing a wheelie!`)
+  }
     // TODO: The method should log the message "Motorbike [make] [model] is doing a wheelie!"
 
   override printDetails(): void {
